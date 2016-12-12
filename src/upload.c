ccae90b5f03cf875de26c2b4368d0113d70970e7
BurningJune <867736454@qq.com>
2016-12-13 00:18:50 +0800

    first

---
+++ src/upload.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include"make_log.h"
+#include<unistd.h>
+
+int main()
+{
+	pid_t pid = 0;
+	int fd[2];
+	char buf[256] = {0};
+	int ret = pipe(fd);
+	if(-10 == ret)
+	{
+		perror("pipe");
+		exit(1);
+	}
+	//重定向输出
+	dup2(fd[1],STDOUT_FILENO);
+	pid = fork();
+	if(pid < 0)
+	{
+		perror("fork");
+		exit(1);
+	}
+	
+	if(0 == pid)
+	{
+		close(fd[0]);
+		execlp("fdfs_upload_file","fdfs_upload_file","/etc/fdfs/client.conf" ,"../2.c",NULL);
+		perror("execlp");
+		exit(1);
+	}
+
+	if(pid > 0)
+	{
+		close(fd[1]);
+		read(fd[0],buf,256);
+		LOG("upload","upload",buf);
+		wait(NULL);
+	}
+
+	return 0;
+}
